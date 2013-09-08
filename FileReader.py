22377125e406dfe2102404ffa9a5ec8fa43ccdaf
shamsazad <shams.azad@gmail.com>
2013-09-08 17:08:52 -0400

    Adding modularity to program

--- FileReader.py
+++ FileReader.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python
 
+def reading_file(fp1):
+	i = 0
+	fp2 = open('sam.txt','r+') 
+	array = []
+	for line in fp1:
+		array.append(line)
+		i=i+1
+	for j in range (i):
+		print(j,array[j])
+
 def main():
 	fp = open('sam.txt','r+')
-	
-	for line in fp:
-		print line 
+	reading_file(fp)	
 	
 	fp.seek(0)
 	c = fp.readlines()
