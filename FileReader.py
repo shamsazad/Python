698eb8b9c90f96ca1b819c3aa5e9d6e6848e028f
shamsazad <shams.azad@gmail.com>
2013-09-08 22:52:46 -0400

    Database connection

--- FileReader.py
+++ FileReader.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python
 
+import psycopg2
+import sys
+
+connectN = None
+
+#function for reading file
 def reading_file(fp1):
 	i = 0
 	fp2 = open('sam.txt','r+') 
@@ -10,9 +16,26 @@ def reading_file(fp1):
 	for j in range (i):
 		print(j,array[j])
 
+
+#function for reading database
+def db_reading():
+	#creating connection with DB
+	try:
+		connectN = psycopg2.connect(database='mapreduce',user='postgres',host='localhost',password='spw666')
+		print "connection successful"
+
+	except psycopg2.DatabaseError, e:
+		print "unable to connect db"
+		print 'Error %s' %e
+
+#main function 
 def main():
 	fp = open('sam.txt','r+')
+	
+	#calling different function
 	reading_file(fp)	
+	db_reading()
+
 	
 	fp.seek(0)
 	c = fp.readlines()
